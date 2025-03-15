96093deb58f6c1020279b74c0795c9a2c5310ad3
Mhyne <magno.259716@globalcity.sti.edu.ph>
2025-03-15 14:51:28 +0800

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div className='flex justify-center items-center min-h-screen text-5xl text-red-500'>Before i let u go can i kiss goodnight baby :)</div>
+  )
+}
+
+export default about
---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <h1 className="text-5xl font-bold text-amber-400">Zhiyuan Enterprise</h1>
+      <h2 className="text-3xl text-white">Where expectations meets reality.</h2>
+    </div>
+  );
+};
+
+export default Hero;
