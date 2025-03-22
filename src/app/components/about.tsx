11b4b2737e2d96462cb9dad9a32884a2213a8ee0
Mhyneski <magno.259716@globalcity.sti.edu.ph>
2025-03-22 20:00:30 +0800

    animation in about done

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -6,21 +6,21 @@ const Details = [
   {
     image: "../images/8ball.png",
     description: "Welcome to Bilmagic, your premier destination for high-quality billiards equipment and accessories. As a brand under Zhiyuan Company.",
-    color: "bg-darkGrayish",
+    color: "bg-[linear-gradient(135deg,#a6a6a6,#ffffff)]",
     size: "text-3xl",
     width: "max-w-[300px]"
   },
   {
     image: "../images/10ball.png",
     description: "Bilmagic is your go-to hub for premium cue sticks and billiards gear. We craft our own Bilmagic-branded cue sticks with precision and expertise, catering to both professionals and enthusiasts.",
-    color: "bg-darkBlueish",
+    color: "bg-[linear-gradient(90deg,#000000,#1584bb)]",
     size: "text-2xl",
     width: "max-w-[280px]"
   },
   {
     image: "../images/3ball.png",
     description: "As a trusted distributor of top brands like Peri, we offer a diverse selection of high-performance cues—ensuring every player finds their perfect match.",
-    color: "bg-lightGrayish",
+    color: "bg-[linear-gradient(90deg,#000000,#737373)]",
     size: "text-3xl",
     width: "max-w-[350px]"
   },
@@ -31,14 +31,22 @@ export default function About() {
     <div className="relative min-h-screen flex flex-col items-center pt-17">
       <img src="../images/rack.png" className='w-470 absolute -right-223 -top-105' alt="rack" />
       <h1 className="text-center text-5xl mt-20 text-white">ABOUT US</h1>
-      <div className="grid grid-cols-1 md:grid-cols-12 gap-35 -ml-93 mt-35 max-w-6xl px-4">
+      <motion.div
+        variants={fadeIn("right", 0)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.7 }} className="grid grid-cols-1 md:grid-cols-12 gap-35 -ml-93 mt-35 max-w-6xl px-4">
         {Details.map((detail, index) => (
-          <div key={index} className={`relative col-span-4 h-[450px] w-[365px] p-6 rounded-3xl shadow-2xl drop-shadow-[0_35px_35px_rgba(0,0,0,0.3)] ${detail.color} flex flex-col items-center text-center hover:scale-130 transition-all duration-300 ease-in-out`}>
-            <img src={detail.image} alt="Billiard Ball" className="absolute -top-44 hover:scale-110 transition-all duration-300 ease-in-out w-200 h-85 object-cover" />
+          <motion.div
+            variants={fadeIn("right", 0)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.7 }} key={index} className={`relative col-span-4 h-[450px] w-[365px] p-6 rounded-3xl shadow-2xl drop-shadow-[0_35px_35px_rgba(0,0,0,0.3)] ${detail.color} flex flex-col items-center text-center hover:scale-130 transition-all duration-300 ease-in-out`}>
+            <img src={detail.image} alt="Billiard Ball" className="absolute -top-44 hover:scale-110 transition-all duration-300 ease-in-out w-50 h-85 object-cover" />
             <p className={`mt-20 ${detail.size} font-medium text-white ${detail.width} text-left`}>{detail.description}</p>
-          </div>
+          </motion.div>
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 }
--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -6,13 +6,14 @@ interface ButtonProps {
   design?: string;
 }
 
-export default function Button ({ text, onClick, design }: ButtonProps) {
-  return (
-    <button
-      onClick={onClick}
-      className={`rounded-full w-[250px] bg-[linear-gradient(90deg,#ffffff,#003968)] px-4 py-4 text-white ${design}`}
-    >
-      {text}
-    </button>
-  );
-}
+export default function Button({ text, onClick, design }: ButtonProps) {
+    return (
+      <button
+        onClick={onClick}
+        className={`relative rounded-full w-[250px] cursor-pointer px-4 py-4 text-white bg-transparent before:absolute before:inset-0 before:rounded-full before:bg-[linear-gradient(90deg,#191834,#2b2c68,#338aca,#26b7cd,#61bdf0)] before:-z-10 ${design}`}
+      >
+        {text}
+      </button>
+    );
+  }
+  
